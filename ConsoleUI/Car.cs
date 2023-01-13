df57835300dd92cb3d25ff619345aa0ea16678ea
Jill Reese <ajreese@gmail.com>
2023-01-13 12:42:59 -0500

    Initial set of class files

---
+++ ConsoleUI/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public class Car : Vehicle
+    {
+        public Car()    
+        {  
+        
+        }
+
+        //Properties
+        bool HasTrunk = true;
+        int NumberOfWheels = 4;
+
+        public override void DriveAbstract()
+        {
+            Console.WriteLine($"Sunroof is open and the weather is clear!");
+        }
+
+        public override void DriveVirtual()
+        {
+                 
+        }
+
+    }
+}
---
+++ ConsoleUI/Motorcycle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public class Motorcycle : Vehicle
+    {
+
+            public Motorcycle()
+            {
+
+            }
+
+            int NumberOfWheels = 2;
+            bool HasWindshield = false;
+
+            public override void DriveAbstract()
+            {
+                Console.WriteLine($"Hope it's not raining!");
+            }
+
+            public override void DriveVirtual()
+            {
+                Console.WriteLine($"Motorcycles feel the wind in their hair virtually!");
+            }
+
+    }
+}
